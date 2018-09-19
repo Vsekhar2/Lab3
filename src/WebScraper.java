1345c9c20823b23091374444bb15025869b27e7d
SvinSoftWare <vinodhsekhar@gmail.com>
2018-09-19 18:25:14 -0500

    Secondary Commit

--- src/WebScraper.java
+++ src/WebScraper.java
@@ -1,8 +1,60 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
 public class WebScraper {
     public static void main(String[] unused) {
         System.out.println("ran");
 
+        String urlString  = urlToString("http://erdani.com/tdpl/hamlet.txt");
+
+        System.out.println(countString(urlString, "Come"));
+
+
+
 
     }
+
+    /**
+     * Retrieve contents from a URL and return them as a string.
+     *
+     * @param url url to retrieve contents from
+     * @return the contents from the url as a string, or an empty string on error
+     */
+    public static String urlToString(final String url) {
+        Scanner urlScanner;
+        try {
+            urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+        } catch (IOException e) {
+            return "";
+        }
+        String contents = urlScanner.useDelimiter("\\A").next();
+        urlScanner.close();
+        return contents;
+    }
+
+
+    public static int countString(String urlContent, String word) {
+       int count = 0;
+       String[] splitArray = urlContent.split(" ");
+
+       for ( int i = 0; i < splitArray.length; i++) {
+
+           if (splitArray[i].toLowerCase().equalsIgnoreCase(word.toLowerCase())) {
+
+               count++;
+
+           }
+
+
+       }
+
+
+        return count;
+
+
+
+    }
+
 
 }
